55c57c8be42522deb24881261b2c725d1d6f2de2
ccagil24 <ccagil24@ku.edu.tr>
2025-10-24 09:45:53 +0300

    Fixed convert_temperature function

--- q2.py
+++ q2.py
@@ -1,9 +1,8 @@
 def convert_temperature(temp,unit):
     if unit == "C":
-        temp = round((9 * temp) / 5 + 32 , 2)
-        return(f"the temperature Fahrenheit is: {temp} F")
+        return round((9 * temp) / 5 + 32 , 2)
     elif unit == "F":
-        temp = round ((temp - 32) * 5 / 9, 2)
-        return(f"the temperature Celsius is: {temp} C")
+        return round ((temp - 32) * 5 / 9, 2)
     else:
-        return("f {unit} is an invalid unit of measurement")
+        return None
+    #convertion
